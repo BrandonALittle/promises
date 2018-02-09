7d3c96e76802d68fc2bcf3b24961adf8448aadc8
BrandonALittle <brandon.a.little@gmail.com>
2018-02-09 08:51:51 -0800

    completed -- stil failing one test -- unsure why

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,29 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf-8', function(err, data) {
+    if (err) {
+      callback(err, null);
+    } else {
+      let lines = data.split('\n');
+      let firstLine = lines[0];
+      callback(null, firstLine);
+    }
+  });// TODO
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request(url, function(err, response, body) {
+    if (err) {
+      err.message = 'Invalid URI';
+      callback(err, null);
+    } else {
+      let statusCode = response.statusCode;
+      callback(null, statusCode);
+    }// TODO
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,12 +10,32 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, 'utf-8', function(err, data) {
+      {
+        if (err) {
+          reject(err);
+        } else {
+          let firstLine = data.split('\n')[0];
+          resolve(firstLine);
+        }
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+    request(url, function(err, response, body) {
+      if (err) {
+        reject(err);
+      } else {
+        let statCode = response.statusCode;
+        resolve(statCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
